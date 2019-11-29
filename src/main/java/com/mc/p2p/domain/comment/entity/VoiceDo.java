f17f59292091bca7fe7cc7095a70c2ae2077ce11
yiran <yirandjk@163.com>
2019-11-29 16:14:35 +0800

    修改情感分析失败则设置为中性，增加注释

--- src/main/java/com/mc/p2p/domain/comment/entity/VoiceDo.java
+++ src/main/java/com/mc/p2p/domain/comment/entity/VoiceDo.java
@@ -33,32 +33,50 @@ import static com.mc.p2p.infrastructure.enums.ResponseEnum.VOICE_COMMENT_CAN_NOT
 @Data
 public class VoiceDo {
 
-    private static AipSpeech SPEECH_CLIENT;
-    private static AipNlp AIP_CLIENT;
-
+    /**
+     * 情感分析验签
+     */
     private static Credential CRED;
+
+    /**
+     * 网络头参数
+     */
     private static HttpProfile HTTP_PROFILE;
+
+    /**
+     * 情感分析客户端参数
+     */
     private static ClientProfile CLIENT_PROFILE;
+
+    /**
+     * 情感分析客户端
+     */
     private static NlpClient NLP_CLIENT;
 
+    /**
+     * 语音识别验签
+     */
     private static Credential CRED2;
+
+    /**
+     * 网络头参数
+     */
     private static HttpProfile HTTP_PROFILE2;
+
+    /**
+     * 语音识别客户端参数
+     */
     private static ClientProfile CLIENT_PROFILE2;
+
+    /**
+     * 语音识别客户端
+     */
     private static AsrClient ASR_CLIENT;
 
+    /**
+     * 参数初始化
+     */
     static {
-        SPEECH_CLIENT = new AipSpeech(APP_ID, API_KEY, SECRET_KEY);
-        AIP_CLIENT = new AipNlp(APP_ID, API_KEY, SECRET_KEY);
-
-        // 可选：设置网络连接参数
-        SPEECH_CLIENT.setConnectionTimeoutInMillis(2000);
-        SPEECH_CLIENT.setSocketTimeoutInMillis(60000);
-
-
-        // 可选：设置网络连接参数
-        AIP_CLIENT.setConnectionTimeoutInMillis(2000);
-        AIP_CLIENT.setSocketTimeoutInMillis(60000);
-
 
         CRED = new Credential("AKIDd9UgmhsxJXcaO2cmYFl6GE2e7HJAd4tX", "b1GJBXing8RZWHrRryynXCh19A1gAORJ");
         CRED2 = new Credential("AKIDsDUVHo5M9k5F9N74X2JJ4ZonVxJP4pkP", "YqsjKelocAXgbGgsyRedOuAA3pNeniO9");
@@ -76,39 +94,83 @@ public class VoiceDo {
         ASR_CLIENT = new AsrClient(CRED2, "ap-shanghai", CLIENT_PROFILE2);
     }
 
+    /**
+     * 语音识别数据
+     */
     private SentenceRecognitionResponse speechData;
+
+    /**
+     * 情感分析数据
+     */
     private SentimentAnalysisResponse nlpData;
+
+    /**
+     * 语音文件路径
+     */
     private String voicePath;
+
+    /**
+     * 语音文件编号
+     */
     private String fileId;
+
+    /**
+     * 情感分析得分
+     */
     private Integer score;
 
+    /**
+     * 日志
+     */
+    private static final Logger log = LoggerFactory.getLogger(VoiceDo.class);
+
+    /**
+     * 构造方法
+     * @param path 语音路径
+     * @param fileId 文件编号
+     */
     public VoiceDo(String path, String fileId) {
         this.voicePath = path;
         this.fileId = fileId;
     }
 
-    private static final Logger log = LoggerFactory.getLogger(VoiceDo.class);
-
+    /**
+     * 语音识别
+     */
     public void setComment() {
         log.info("voice recognize start");
 
         try {
-            String params = "{\"ProjectId\":1165413,\"SubServiceType\":2,\"EngSerViceType\":\"16k\",\"SourceType\":1,\"VoiceFormat\":\"wav\",\"UsrAudioKey\":\"%s\",\"Data\":\"%s\",\"DataLen\":%d}";
+            String params = "{\"ProjectId\":1165413," +
+                    "\"SubServiceType\":2," +
+                    "\"EngSerViceType\":\"16k\"," +
+                    "\"SourceType\":1," +
+                    "\"VoiceFormat\":\"wav\"," +
+                    "\"UsrAudioKey\":\"%s\"," +
+                    "\"Data\":\"%s\"," +
+                    "\"DataLen\":%d}";
             byte[] data = Util.readFileByBytes(this.voicePath);
             String voiceString = new BASE64Encoder().encode(reSamplingPCM(data));
-            params = String.format(params, UUID.randomUUID().toString(), voiceString, voiceString.length());
-            SentenceRecognitionRequest req = SentenceRecognitionRequest.fromJsonString(params, SentenceRecognitionRequest.class);
+            params = String.format(params, UUID.randomUUID().toString(), voiceString,
+                    voiceString.length());
+            SentenceRecognitionRequest req = SentenceRecognitionRequest
+                    .fromJsonString(params, SentenceRecognitionRequest.class);
             SentenceRecognitionResponse resp = ASR_CLIENT.SentenceRecognition(req);
             this.speechData = resp;
             System.out.println(speechData);
-            setScore();
             log.info("voice recognize finished");
+            setScore();
         } catch (Exception e) {
             log.error("voice recognize error，exception is {}", e.getStackTrace());
             throw new BusinessException(VOICE_COMMENT_CAN_NOT_RECOGNIZED);
         }
     }
 
+    /**
+     * 语音文件转码
+     * @param data 文件字节码
+     * @return 转码后的字节码
+     */
     private static byte[] reSamplingPCM(byte[] data) {
 
         log.info("reSampling start");
@@ -133,25 +195,31 @@ public class VoiceDo {
         }
     }
 
-    public void setScore() {
+    /**
+     * 情感分析
+     */
+    private void setScore() {
         try {
             log.info("sentiment analyze start");
             String params = "{\"Text\":\"%s\"}";
             String texts = this.speechData.getResult();
             params = String.format(params, texts);
-
             System.out.println(params);
-            SentimentAnalysisRequest req = SentimentAnalysisRequest.fromJsonString(params, SentimentAnalysisRequest.class);
-
+            SentimentAnalysisRequest req = SentimentAnalysisRequest
+                    .fromJsonString(params, SentimentAnalysisRequest.class);
             SentimentAnalysisResponse resp = NLP_CLIENT.SentimentAnalysis(req);
-
             System.out.println(resp);
             this.nlpData = resp;
             this.score = BigDecimal.valueOf(resp.getPositive()).movePointRight(1).intValue();
             log.info("sentiment analyze success");
         } catch (Exception e) {
+            SentimentAnalysisResponse response = new SentimentAnalysisResponse();
+            response.setNegative(0.5F);
+            response.setPositive(0.5F);
+            response.setSentiment("neutral");
+            this.setNlpData(response);
+            this.score = 5;
             log.error("sentiment analyze error, exception is {}", e.getStackTrace());
-            throw new BusinessException(VOICE_COMMENT_CAN_NOT_RECOGNIZED);
         }
     }
 }
