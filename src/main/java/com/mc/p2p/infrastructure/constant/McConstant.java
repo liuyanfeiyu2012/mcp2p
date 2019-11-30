da4c49b7a6eb49e71e3f61b63a7e4dce36055de9
xingxing.xie <413151628@qq.com>
2019-11-30 09:28:46 +0800

    modify

--- src/main/java/com/mc/p2p/infrastructure/constant/McConstant.java
+++ src/main/java/com/mc/p2p/infrastructure/constant/McConstant.java
@@ -19,10 +19,12 @@ public class McConstant {
      * AI_APP_ID
      */
     public static final String AI_APP_ID = "17884831";
+
     /**
      * AI_API_KEY
      */
     public static final String AI_API_KEY = "sEIZGTv1n7jaydDxPAyYxZUh";
+
     /**
      * AI_SECRET_KEY
      */
@@ -34,21 +36,25 @@ public class McConstant {
      */
     public static final String FILE_VIDEO_PATH
             = "/home/ubuntu/mcp2p/file/video/";
+
     /**
      * FILE_VOICE_PATH
      */
     public static final String FILE_VOICE_PATH
             = "/home/ubuntu/mcp2p/file/voice/";
+
     /**
      * FILE_BG_PATH
      */
     public static final String FILE_BG_PATH
             = "/home/ubuntu/mcp2p/file/bg/";
+
     /**
      * FILE_BGM_PATH
      */
     public static final String FILE_BGM_PATH
             = "/home/ubuntu/mcp2p/file/bgm/";
+
     /**
      * FILE_DISCERN_PATH
      */
@@ -59,26 +65,32 @@ public class McConstant {
      * FFMPEG_DO_KEY
      */
     public static final String FFMPEG_DO_KEY = "FFPEG_DO_KEY";
+
     /**
      * CONVERT_KEY
      */
     public static final String CONVERT_KEY = "CONVERT_KEY";
+
     /**
      * MIX_BGM_KEY
      */
     public static final String MIX_BGM_KEY = "MIX_BGM_KEY";
+
     /**
      * CANCEL_BGM_KEY
      */
     public static final String CANCEL_BGM_KEY = "CANCEL_BGM_KEY";
+
     /**
      * SCREENSHOT_KEY
      */
     public static final String SCREENSHOT_KEY = "SCREENSHOT_KEY";
+
     /**
      * COMPRESS_KEY
      */
     public static final String COMPRESS_KEY = "COMPRESS_KEY";
+
     /**
      * ADD_WATER_KEY
      */
@@ -88,10 +100,12 @@ public class McConstant {
      * MP4_EXT
      */
     public static final String MP4_EXT = ".mp4";
+
     /**
      * JPG_EXT
      */
     public static final String JPG_EXT = ".jpg";
+
     /**
      * WAV_EXT
      */
@@ -101,10 +115,12 @@ public class McConstant {
      * MAX_LIMIT
      */
     public static final double MAX_LIMIT = 20;
+
     /**
      * MIN_LIMIT
      */
     public static final double MIN_LIMIT = 4;
+
     /**
      * VIDEO_NGINX_PREFFIX
      */
@@ -119,18 +135,25 @@ public class McConstant {
     /**
      * CAT_PRODUCT
      */
-    public static final List<String> CAT_PRODUCT = Lists.newArrayList(
-            "猫罐头", "进口猫粮", "猫砂", "逗猫棒", "猫猫小鱼干");
+    public static final List<String> CAT_PRODUCT = Lists.newArrayList("猫罐头",
+            "进口猫粮",
+            "猫砂",
+            "逗猫棒",
+            "猫猫小鱼干");
+
     /**
      * DOG_PRODUCT
      */
-    public static final List<String> DOG_PRODUCT = Lists.newArrayList(
-            "进口狗粮", "狗狗磨牙棒", "狗星人飞盘", "小狗皮球");
+    public static final List<String> DOG_PRODUCT =
+            Lists.newArrayList("进口狗粮", "狗狗磨牙棒",
+                    "狗星人飞盘", "小狗皮球");
+
     /**
      * FISH_PRODUCT
      */
     public static final List<String> FISH_PRODUCT = Lists.newArrayList(
             "鱼食", "小鱼纪念品", "鱼竿", "钓鱼套装");
+
     /**
      * NOMAL_PRODUCT
      */
@@ -142,84 +165,102 @@ public class McConstant {
      * AI_SC_NUM
      */
     public static final int AI_SC_NUM = 3;
+
     /**
      * CATE_A
      */
     public static final String CATE_A = "猫";
+
     /**
      * DOG_A
      */
     public static final String DOG_A = "柯基";
+
     /**
      * DOG_B
      */
     public static final String DOG_B = "哈士奇";
+
     /**
      * DOG_C
      */
     public static final String DOG_C = "阿拉斯加";
+
     /**
      * DOG_D
      */
     public static final String DOG_D = "萨摩耶";
+
     /**
      * DOG_E
      */
     public static final String DOG_E = "拉布拉多";
+
     /**
      * DOG_F
      */
     public static final String DOG_F = "吉娃娃";
+
     /**
      * DOG_G
      */
     public static final String DOG_G = "泰迪";
+
     /**
      * DOG_H
      */
     public static final String DOG_H = "梗";
+
     /**
      * DOG_I
      */
     public static final String DOG_I = "獒";
+
     /**
      * DOG_J
      */
     public static final String DOG_J = "狗";
+
     /**
      * DOG_K
      */
     public static final String DOG_K = "犬";
+
     /**
      * FISH_A
      */
     public static final String FISH_A = "鱼";
+
     /**
      * NO_ANIMAL
      */
     public static final String NO_ANIMAL = "非动物";
 
-
     /**
      * SAMPLE_RATE
      */
     public static final int SAMPLE_RATE = 16000;
+
     /**
      * BYTES_PER_SAMPLE
      */
     private static final int BYTES_PER_SAMPLE = 2;
+
     /**
      * BITS_PER_SAMPLE
      */
     private static final int BITS_PER_SAMPLE = 16;
+
     /**
      * MONO
      */
     private static final int MONO = 1;
+
     /**
      * LITTLE_ENDIAN
      */
     private static final boolean LITTLE_ENDIAN = false;
+
     /**
      * AUDIO_FORMAT
      */
